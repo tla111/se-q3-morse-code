d645cad87d30c17477d4704df2e0e3bc3cc3c1a5
Tim La <email@example.com>
2020-08-23 20:30:58 -0400

    added author name

--- morse.py
+++ morse.py
@@ -9,7 +9,7 @@ Pause between dots and dashes in a character – is 1 time unit long.
 Pause between characters inside a word – is 3 time units long.
 Pause between words – is 7 time units long.
 """
-__author__ = '???'
+__author__ = 'Timothy La (tla111'
 
 from morse_dict import MORSE_2_ASCII
 
@@ -26,7 +26,7 @@ def decode_morse(morse):
 
 if __name__ == '__main__':
     hey_jude_morse = ".... . -.--   .--- ..- -.. ."
-    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011" # noqa
+    hey_jude_bits = "11001100110011000000110000001111110011001111110011111100000000000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"  # noqa
 
     # Be sure to run all included unit tests, not just this one.
     print("Morse Code decoder test")
