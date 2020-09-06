b43e074f93d21945c326a3ab5bbf1382aed3e95e
Tim La <email@example.com>
2020-09-06 13:49:55 -0400

    commented out code

--- review.py
+++ review.py
@@ -1,28 +1,51 @@
 from morse_dict import MORSE_2_ASCII
 
+# This function will transcode (covert encoded date) the binary
+#   sequence into Morse code representation
 
-# def decode_bits(bits):
-#     bits = bits.strip("0")
-#     freq = min([len(bit) for bit in bits.split("1")
-#     + bits.split("0") if bit])
 
-#     bits_morsecode = bits.replace(
-#         "111" * freq, "-").replace(
-#             "1" * freq, ".").replace(
-#                 "0000000" * freq, "   ").replace(
-#                     "000" * freq, " ").replace(
-#                         "0" * freq, "")
+def decode_bits(bits):
+    # Take off any 0's in the front or back of string
+    # In this case, no need to since both ends have 1s
+    bits = bits.strip("0")
+    # 1. If bit is true
+    #       Run the for loop
+    # 2. Get the length of each bit and store in list
+    # 3. Output the value of the minimum number of occurrence
+    freq = min([len(bit) for bit in bits.split("1")
+                + bits.split("0") if bit])
 
-#     return bits_morsecode.strip()
+    # Multiply the "string" by the freq (2) &
+    #   replace it with a new value
+    # Ex: "111" * 2(freq) = "111111" ->
+    #   Replace "111111" to "-"
+    # Ex: "1" * 2(freq) = "11" ->
+    #   Replace "11" to "."
+    bits_morsecode = bits.replace(
+        "111" * freq, "-").replace(
+            "1" * freq, ".").replace(
+                "0000000" * freq, "   ").replace(
+                    "000" * freq, " ").replace(
+                        "0" * freq, "")
+
+    # Return bits_morsecode
+    #   Leading and trailing whitespaces are removed
+    return bits_morsecode.strip()
+
+
+hey_jude_bits = "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011"
+print(decode_bits(hey_jude_bits))
 
 
 # This function will decode the morse code (..-..) to words
+
+
 def decode_morse(morse):
     # Create a "" as a key in MORSE_2_ASCII
     # & store " " as the value
     # Ex: {"": " "}
     MORSE_2_ASCII[""] = " "
-    # Store an empty string in the variable, result
+    # Store an empty string in a variable (result)
     result = ""
     # The split() breaks up a string at the specified
     #   separator and return a list of strings
@@ -58,4 +81,4 @@ def decode_morse(morse):
     return " ".join(result.split())
 
 
-decode_morse(".... . -.--   .--- ..- -.. .")
+# decode_morse(".... . -.--   .--- ..- -.. .")
